cf0a441232732093cd8223e067a13f29f43b0512
savindu-pasintha <savindupasingtha@gmail.com>
2025-05-03 00:02:21 +0800

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.3"
+	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.4"
 	id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
 }
